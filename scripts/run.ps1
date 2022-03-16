14c071ccfffe94b73c82dd3cd63384f25733d05d
RomainTHD <romain-thd+github@protonmail.com>
2022-03-16 14:34:48 +0100

    fix: Better scripts

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,3 +1,3 @@
 # It is assumed that the current working directory is the root, NOT the scripts subdirectory
-$testPath=$args[0]
+$testPath = $args[0]
 wsl.exe sh -c "export CLASSPATH=.:/usr/share/java/JLex.jar:/usr/share/java/cup.jar; ./jlc $testPath"
