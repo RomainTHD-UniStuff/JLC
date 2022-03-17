c7e302c8c71aef050aa9189346f338510ab9ec9f
RomainTHD <romain-thd+github@protonmail.com>
2022-03-17 15:50:40 +0100

    fix: Script message when done

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,4 @@
 # It is assumed that the current working directory is the root, NOT the scripts subdirectory
 $filename = $args[0]
 wsl.exe sh -c "export CLASSPATH=.:/usr/share/java/JLex.jar:/usr/share/java/cup.jar; make all; mv submission.tar.gz $filename; cd ./tester; python3 ./testing.py ../$filename --sh"
+Write-Host "Done." -ForegroundColor Green
