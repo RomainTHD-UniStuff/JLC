75cf6da5385c4c3dd28a922fac94a3e240dcf243
RomainTHD <romain-thd+github@protonmail.com>
2022-03-19 14:59:07 +0100

    update: Added more tests

--- tester/testsuite/bad/bad067.jl
+++ tester/testsuite/bad/bad067.jl
@@ -1,4 +1,22 @@
-// return type must be int
-void main() {
-    return 0;
+// Testing the return checker
+
+int f () {
+   if (true)
+     return 0;
+   else
+     {}
+}
+
+int g () {
+  if (false)
+      {}
+  else
+      return 0;
+}
+
+void p () {}
+
+int main() {
+  p();
+  return 0;
 }
