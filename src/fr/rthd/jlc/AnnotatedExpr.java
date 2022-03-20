ec20774c8ae652ce1bee1762b3bbccbb6756d3ef
RomainTHD <romain-thd+github@protonmail.com>
2022-03-20 17:40:16 +0100

    clean: Unused code

--- src/fr/rthd/jlc/AnnotatedExpr.java
+++ src/fr/rthd/jlc/AnnotatedExpr.java
@@ -5,12 +5,10 @@ import javalette.Absyn.Expr;
 public class AnnotatedExpr<T extends Expr> extends Expr {
     public final T parentExp;
     public TypeCode type;
-    public TypeCode coertTo;
 
     public AnnotatedExpr(TypeCode expType, T parentExp) {
         this.type = expType;
         this.parentExp = parentExp;
-        this.coertTo = null;
     }
 
     @Override
