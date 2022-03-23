1ceb31e73de3b725aa3279f60a6d95b39340a7a8
RomainTHD <romain-thd+github@protonmail.com>
2022-03-23 18:05:20 +0100

    refactor: Unused fields

--- src/fr/rthd/jlc/TypeCode.java
+++ src/fr/rthd/jlc/TypeCode.java
@@ -1,28 +1,22 @@
 package fr.rthd.jlc;
 
 public enum TypeCode {
-    CInt("i32", 'I', 1),
-    CDouble("double", 'D', 2),
-    CBool("i1", 'I', 1),
-    CVoid("void", 'V', 0),
-    CString("i8*", 'S', 0);
+    CInt("i32", 1),
+    CDouble("double", 2),
+    CBool("i1", 1),
+    CVoid("void", 0),
+    CString("i8*", 0);
 
     private final String _typename;
-    private final char _shortType;
     private final int _size;
 
-    TypeCode(String typename, char shortType, int size) {
+    TypeCode(String typename, int size) {
         this._typename = typename;
-        this._shortType = shortType;
         this._size = size;
     }
 
     public String toString() {
         return this._typename;
-    }
-
-    public char getShortType() {
-        return this._shortType;
     }
 
     public int getSize() {
