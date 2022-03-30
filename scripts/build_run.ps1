06170e01e113f157111b0a1d10bbee8fc5267ea3
RomainTHD <romain-thd+github@protonmail.com>
2022-03-30 16:42:12 +0200

    refactor: Duplicated code

--- scripts/build_run.ps1
+++ scripts/build_run.ps1
@@ -1,7 +1,4 @@
 # It is assumed that the current working directory is the root, NOT the scripts subdirectory
 $testPath = $args[0]
-wsl.exe sh -c "
-export CLASSPATH=.:./lib/JLex.jar:./lib/cup.jar;
-make || exit;
-cat $testPath | ./jlc;
-"
+./scripts/build.ps1
+./scripts/singletest.ps1 $testPath
