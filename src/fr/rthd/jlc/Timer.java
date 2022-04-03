ca6174eb6ff977e355a38469ade49e54ac59e778
RomainTHD <romain-thd+github@protonmail.com>
2022-04-03 17:29:36 +0200

    feat: Timer for debugging

---
+++ src/fr/rthd/jlc/Timer.java
@@ -0,0 +1,30 @@
+package fr.rthd.jlc;
+
+public class Timer {
+    private long start;
+    private long end;
+
+    public Timer start() {
+        start = System.nanoTime();
+        return this;
+    }
+
+    public long stop() {
+        end = System.nanoTime();
+        return getTime();
+    }
+
+    public long getTime() {
+        return (end - start) / 1_000;
+    }
+
+    public long clock() {
+        return (System.nanoTime() - start) / 1_000;
+    }
+
+    public long reset() {
+        long time = clock();
+        start();
+        return time;
+    }
+}
