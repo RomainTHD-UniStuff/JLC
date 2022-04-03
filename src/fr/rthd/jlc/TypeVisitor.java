c8f65163ac4e2db9e1d6006a56efd4055a43124e
RomainTHD <romain-thd+github@protonmail.com>
2022-04-03 17:35:40 +0200

    refactor: Unused grammar rule

--- src/fr/rthd/jlc/TypeVisitor.java
+++ src/fr/rthd/jlc/TypeVisitor.java
@@ -2,7 +2,6 @@ package fr.rthd.jlc;
 
 import javalette.Absyn.Bool;
 import javalette.Absyn.Doub;
-import javalette.Absyn.Fun;
 import javalette.Absyn.Int;
 import javalette.Absyn.Type;
 
@@ -21,9 +20,5 @@ public class TypeVisitor implements Type.Visitor<TypeCode, Void> {
 
     public TypeCode visit(javalette.Absyn.Void t, Void ignored) {
         return TypeCode.CVoid;
-    }
-
-    public TypeCode visit(Fun p, Void ignored) {
-        throw new UnsupportedOperationException("visit(javalette.Absyn.Fun)");
     }
 }
