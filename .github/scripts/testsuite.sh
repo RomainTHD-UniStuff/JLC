890ec93257588719176a4a134cb2b8fa00103455
RomainTHD <romain-thd+github@protonmail.com>
2022-04-06 15:55:57 +0200

    ci: Automatically run testsuite

---
+++ .github/scripts/testsuite.sh
@@ -0,0 +1,7 @@
+pwd;
+export CLASSPATH=.:./lib/JLex.jar:./lib/cup.jar
+make all || exit
+mv submission.tar.gz partA-1.tar.gz
+cd ./tester || exit
+export CLASSPATH=.:./lib/JLex.jar:./lib/cup.jar:../lib/JLex.jar:../lib/cup.jar
+python3 ./testing.py ../partA-1.tar.gz --llvm
