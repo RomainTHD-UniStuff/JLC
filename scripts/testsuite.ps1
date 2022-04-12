babaf3e905f8b00ade085cec1659ebcf306008fe
RomainTHD <romain-thd+github@protonmail.com>
2022-04-12 16:37:39 +0200

    feat: OOP testsuite

--- scripts/testsuite.ps1
+++ scripts/testsuite.ps1
@@ -6,6 +6,6 @@ make all || exit;
 mv submission.tar.gz $filename;
 cd ./tester;
 export CLASSPATH=.:./lib/JLex.jar:./lib/cup.jar:../lib/JLex.jar:../lib/cup.jar;
-python3 ./testing.py ../$filename --llvm;
+python3 ./testing.py ../$filename --llvm -x objects1;
 "
 Write-Host "Done." -ForegroundColor Green
