0ceb117c808f7e53a51603f2685f7748ca7170e5
RomainTHD <romain-thd+github@protonmail.com>
2022-04-18 21:56:00 +0200

    feat: Current class stored in environment

--- src/fr/rthd/jlc/typecheck/EnvTypecheck.java
+++ src/fr/rthd/jlc/typecheck/EnvTypecheck.java
@@ -7,14 +7,16 @@ import fr.rthd.jlc.env.FunType;
 
 /**
  * Environment for typechecker
- * @see Env
  * @author RomainTHD
+ * @see Env
  */
 class EnvTypecheck extends Env<TypeCode, FunType, ClassType> {
     /**
      * Current function type
      */
-    public TypeCode currentFunctionType = null;
+    private FunType _currentFunction = null;
+
+    private ClassType _currentClass = null;
 
     /**
      * The function does return or not
@@ -31,5 +33,25 @@ class EnvTypecheck extends Env<TypeCode, FunType, ClassType> {
 
     public void setReturn(boolean doesReturn) {
         this._doesReturn = doesReturn;
+    }
+
+    public FunType getCurrentFunction() {
+        return _currentFunction;
+    }
+
+    public void setCurrentFunction(FunType currentFunction) {
+        this._currentFunction = currentFunction;
+    }
+
+    public ClassType getCurrentClass() {
+        return _currentClass;
+    }
+
+    public void setCurrentClass(ClassType currentClass) {
+        this._currentClass = currentClass;
+    }
+
+    public void clearCurrentClass() {
+        this._currentClass = null;
     }
 }
