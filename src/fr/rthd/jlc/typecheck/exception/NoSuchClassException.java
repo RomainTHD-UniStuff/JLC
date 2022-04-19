70c058c4401fd5961a0ef1d7392b30a2acfa9b75
RomainTHD <romain-thd+github@protonmail.com>
2022-04-19 18:28:07 +0200

    feat: Class not defined exception

---
+++ src/fr/rthd/jlc/typecheck/exception/NoSuchClassException.java
@@ -0,0 +1,17 @@
+package fr.rthd.jlc.typecheck.exception;
+
+import fr.rthd.jlc.TypeCode;
+
+/**
+ * The class does not exist
+ * @author RomainTHD
+ */
+public class NoSuchClassException extends TypeException {
+    public NoSuchClassException(TypeCode t) {
+        this(t.getRealName());
+    }
+
+    public NoSuchClassException(String s) {
+        super("Class not found: " + s);
+    }
+}
