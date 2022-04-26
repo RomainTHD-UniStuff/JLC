210e7fe66a8dcc1cb3dd06cb2b5c02ad719e414f
RomainTHD <romain-thd+github@protonmail.com>
2022-04-26 17:11:43 +0200

    refactor: Method location

--- src/fr/rthd/jlc/TypeVisitor.java
+++ src/fr/rthd/jlc/TypeVisitor.java
@@ -5,32 +5,56 @@ import javalette.Absyn.Class;
 import javalette.Absyn.Doub;
 import javalette.Absyn.Int;
 import javalette.Absyn.Type;
+import javalette.Absyn.Void;
+
+import static fr.rthd.jlc.TypeCode.CBool;
+import static fr.rthd.jlc.TypeCode.CDouble;
+import static fr.rthd.jlc.TypeCode.CInt;
+import static fr.rthd.jlc.TypeCode.CVoid;
 
 /**
  * Type visitor to transform a Javalette type to a TypeCode
  * @author RomainTHD
  */
 public class TypeVisitor implements Type.Visitor<TypeCode, Void> {
+    /**
+     * Javalette type from TypeCode
+     * @param t TypeCode type
+     * @return Javalette type
+     * @throws IllegalArgumentException If type is not supported
+     * @see TypeCode
+     */
+    public static Type fromTypecode(TypeCode t) throws IllegalArgumentException {
+        if (CVoid.equals(t)) {
+            return new Void();
+        } else if (CBool.equals(t)) {
+            return new Bool();
+        } else if (CInt.equals(t)) {
+            return new Int();
+        } else if (CDouble.equals(t)) {
+            return new Doub();
+        } else if (t.isObject()) {
+            return new Class(t.getRealName());
+        }
+
+        throw new IllegalArgumentException("Unknown typecode: " + t);
+    }
+
     public TypeCode visit(Bool t, Void ignored) {
-        return TypeCode.CBool;
+        return CBool;
     }
 
     public TypeCode visit(Int t, Void ignored) {
-        return TypeCode.CInt;
+        return CInt;
     }
 
     public TypeCode visit(Doub t, Void ignored) {
-        return TypeCode.CDouble;
+        return CDouble;
     }
 
     public TypeCode visit(javalette.Absyn.Void t, Void ignored) {
-        return TypeCode.CVoid;
+        return CVoid;
     }
-
-    /*
-    public TypeCode visit(Array t, Void ignored) {
-        return TypeCode.forArray(t.type_.accept(this, null));
-    }*/
 
     public TypeCode visit(Class t, Void ignored) {
         return TypeCode.forClass(t.ident_);
--- src/fr/rthd/jlc/compiler/llvm/LLVMCompiler.java
+++ src/fr/rthd/jlc/compiler/llvm/LLVMCompiler.java
@@ -1,24 +1,13 @@
 package fr.rthd.jlc.compiler.llvm;
 
-import fr.rthd.jlc.TypeCode;
 import fr.rthd.jlc.Visitor;
 import fr.rthd.jlc.env.ClassType;
 import fr.rthd.jlc.env.Env;
 import fr.rthd.jlc.env.FunType;
-import javalette.Absyn.Bool;
-import javalette.Absyn.Doub;
-import javalette.Absyn.Int;
 import javalette.Absyn.Prog;
-import javalette.Absyn.Type;
-import javalette.Absyn.Void;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
-import static fr.rthd.jlc.TypeCode.CBool;
-import static fr.rthd.jlc.TypeCode.CDouble;
-import static fr.rthd.jlc.TypeCode.CInt;
-import static fr.rthd.jlc.TypeCode.CVoid;
 
 /**
  * Compiler
@@ -29,30 +18,6 @@ public class LLVMCompiler implements Visitor {
 
     public LLVMCompiler(String output) {
         this._outputFile = output;
-    }
-
-    /**
-     * Javalette type from TypeCode
-     * @param type TypeCode type
-     * @return Javalette type
-     * @throws IllegalArgumentException If type is not supported
-     * @see TypeCode
-     */
-    private static Type javaletteTypeFromTypecode(TypeCode type) throws IllegalArgumentException {
-        if (CInt.equals(type)) {
-            return new Int();
-        } else if (CDouble.equals(type)) {
-            return new Doub();
-        } else if (CBool.equals(type)) {
-            return new Bool();
-        } else if (CVoid.equals(type)) {
-            return new Void();
-        } else {
-            throw new IllegalArgumentException(
-                "Unsupported type: " +
-                type
-            );
-        }
     }
 
     /**
